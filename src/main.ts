9b001b2b34a3f1d081419b6fdfc3f4b54853bb84
Мечислав Вокалюк <mechia@mail.ru>
2022-07-23 20:14:00 +0300

    fix: fix swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
+import { cwd as getRootPath } from 'node:process';
+
+import { readFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const rootDir = getRootPath();
+
+  const swaggerFile = await readFile(`${rootDir}/doc/api.yaml`);
+  const swaggerDocument = parse(swaggerFile.toString());
+
+  SwaggerModule.setup('swagger', app, swaggerDocument);
+
   await app.listen(4000);
 }
 bootstrap();
